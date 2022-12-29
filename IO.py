2b5c97673afb93e4abad31c3334890a6a7891ee5
gabarnettjr <gabarnettjr@gmail.com>
2022-12-29 12:26:57 -0700

    Create IO.py

---
+++ IO.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+
+################################################################################
+
+def loadArray(fileName) :
+    x = np.array([])
+    with open(fileName) as fh :
+        for line in fh :
+            ell = line.strip()
+            x = np.hstack((x, np.float64(ell)))
+    return x
+
+################################################################################
+
+def saveArray(fileName, values) :
+    with open(fileName, "w") as fh :
+        for i in range(len(values)) :
+            fh.write('{0:17.14f}\n'.format(values[i]))
+

6f9c1307d7c28a9e3838628f070551901ecf78e4
gabarnettjr <gabarnettjr@gmail.com>
2023-01-01 10:36:00 -0700

    Update getFunctionValues.pl

--- getFunctionValues.pl
+++ getFunctionValues.pl
@@ -1,1 +1,78 @@
-asdf
+# windows
+
+# If you already have defined nodes (x, y) and evaluation points (xe, ye), then
+# this script will use a smooth function to define data at these points.
+# The script generates f.txt to go with (x, y), and fe.txt to go with (xe, ye).
+# In order for this to work, you need to already have x.txt, y.txt, xe.txt, and
+# ye.txt saved in the folder at location $dataDir.
+
+# Greg Barnett
+# December 2022
+
+################################################################################
+
+use strict;
+use warnings;
+
+use Math::Trig;
+
+use lib ".";
+use io;
+use linalg;
+
+################################################################################
+
+my ($dataDir, $f);
+
+$dataDir = "randomCoords\\smoothData";
+$f = "f";
+
+if (scalar @ARGV) {
+    $dataDir = shift;
+}
+if (scalar @ARGV) {
+    $f = shift;
+}
+
+################################################################################
+
+my $x = io::loadArray("$dataDir\\..\\x.txt");
+my $y = io::loadArray("$dataDir\\..\\y.txt");
+
+my $xe = io::loadArray("$dataDir\\..\\xe.txt");
+my $ye = io::loadArray("$dataDir\\..\\ye.txt");
+
+################################################################################
+
+sub f {
+	my ($x, $y) = @_;
+	my $z;
+	if ($f eq "f") {
+		$z = cos(2*pi * $x / 25000) * sin(2*pi * $y / 25000);
+	} elsif ($f eq "g") {
+		$z = cos(2*pi * $x / 25000) * cos(2*pi * $y / 25000)
+	       + sin(2*pi * $x / 25000) * sin(2*pi * $y / 25000);
+	} elsif ($f eq "h") {
+		$z = cos(2*pi * $x / 50000) * cos(2*pi * $y / 50000)
+	       + sin(2*pi * $x / 50000) * sin(2*pi * $y / 50000);
+	} else {
+		print STDERR "Invalid choice for \$f.  Choose f, g, or h.\n"; die;
+	}
+	return $z;
+}
+
+sub eff {
+	my ($x, $y) = @_;
+	my $z = linalg::zeros(scalar @{$x});
+	for (my $i = 0; $i < (scalar @{$x}); $i++) {
+		@{$z}[$i] = f(@{$x}[$i], @{$y}[$i]);
+	}
+	return $z;
+}
+
+################################################################################
+
+io::saveArray("$dataDir\\f.txt", eff($x, $y));
+
+io::saveArray("$dataDir\\fe.txt", eff($xe, $ye));
+

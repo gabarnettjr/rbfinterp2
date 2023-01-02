a710967577a680efe6fc9c5fa8c829090cc164ae
gabarnettjr <gabarnettjr@gmail.com>
2023-01-02 13:18:18 -0700

    Add files via upload

--- getFuncVals.pl
+++ getFuncVals.pl
@@ -22,10 +22,10 @@ use linalg;
 
 ################################################################################
 
-my ($dataDir, $f);
+my ($dataDir, $ftype);
 
 $dataDir = "randomCoords\\smoothData";
-$f = "f";
+$ftype = "peaks and valleys";
 
 if (scalar @ARGV) {
 	$dataDir = shift;
@@ -35,51 +35,65 @@ if (scalar @ARGV) {
 		print STDERR "Data directory must contain function values.\n"; die;
 	}
 }
-if (scalar @ARGV) { $f = shift; }
+if (scalar @ARGV) { $ftype = (lc shift); }
 
 ################################################################################
 
 my $x = io::loadArray("$dataDir\\..\\x.txt");
 my $y = io::loadArray("$dataDir\\..\\y.txt");
-
 my $xe = io::loadArray("$dataDir\\..\\xe.txt");
 my $ye = io::loadArray("$dataDir\\..\\ye.txt");
+
+my @ab = (@{$x}, @{$xe});
+my $a = linalg::min(\@ab);
+my $b = linalg::max(\@ab);
+my @cd = (@{$y}, @{$ye});
+my $c = linalg::min(\@cd);
+my $d = linalg::max(\@cd);
 
 ################################################################################
 
 sub f {
-	my ($x, $y) = @_;
-	my $z;
-	if ($f eq "f") {
-		$z = (cos 2*pi * $x / 25000) * (sin 2*pi * $y / 25000);
-	} elsif ($f eq "g") {
-		$z = (cos 2*pi * $x / 25000) * (cos 2*pi * $y / 25000)
-	       + (sin 2*pi * $x / 25000) * (sin 2*pi * $y / 25000);
-	} elsif ($f eq "h") {
-		$z = (cos 2*pi * $x / 50000) * (cos 2*pi * $y / 50000)
-	       + (sin 2*pi * $x / 50000) * (sin 2*pi * $y / 50000);
-	} elsif ($f eq "i") {
-		$z = (exp -(1/8000)**2 * (($x - 10000)**2 + ($y - 35000)**2))
-		   + (exp -(1/8000)**2 * (($x - 32000)**2 + ($y - 5000)**2))
-		   + (exp -(1/5000)**2 * (($x - 41000)**2 + ($y - 38000)**2));
-	} else {
-		print STDERR "Invalid choice for \$f.  Choose f, g, or h.\n"; die;
-	}
-	return $z;
+    my ($ftype, $x, $y, $a, $b, $c, $d) = @_;
+    my $w = ($b - $a);
+    my $ell = ($d - $c);
+    my $s = ($w + $ell) / 2;
+    my $z;
+    if ($ftype eq "peaks and valleys" || $ftype eq "1") {
+        $z = (cos 2*pi * $x / ($w/2)) * (sin 2*pi * $y / ($ell/2));
+    } elsif ($ftype eq "narrow stripes" || $ftype eq "2") {
+        $z = (cos 2*pi * $x / ($w/2)) * (cos 2*pi * $y / ($ell/2))
+           + (sin 2*pi * $x / ($w/2)) * (sin 2*pi * $y / ($ell/2));
+    } elsif ($ftype eq "wide stripes" || $ftype eq "3") {
+        $z = (cos 2*pi * $x / $w) * (cos 2*pi * $y / $ell)
+           + (sin 2*pi * $x / $w) * (sin 2*pi * $y / $ell);
+    } elsif ($ftype eq "bells" || $ftype eq "4") {
+        $z = (exp -(1/(.16*$s))**2 * (($x - ($a + .20*$w))**2 + ($y - ($c + .70*$ell))**2))
+           + (exp -(1/(.16*$s))**2 * (($x - ($a + .64*$w))**2 + ($y - ($c + .10*$ell))**2))
+           + (exp -(1/(.10*$s))**2 * (($x - ($a + .82*$w))**2 + ($y - ($c + .76*$ell))**2));
+    } else {
+        my $s = "Invalid choice for \$ftype.  Try again using one of these:\n";
+        $s .= "(1) \"peaks and valleys\"\n";
+        $s .= "(2) \"narrow stripes\"\n";
+        $s .= "(3) \"wide stripes\"\n";
+        $s .= "(4) \"bells\"\n";
+        print STDERR $s; die;
+    }
+    return $z;
 }
 
 sub eff {
-	my ($x, $y) = @_;
-	my $z = linalg::zeros(scalar @{$x});
-	for (my $i = 0; $i < (scalar @{$x}); $i++) {
-		@{$z}[$i] = f(@{$x}[$i], @{$y}[$i]);
-	}
-	return $z;
+    my ($ftype, $x, $y, $a, $b, $c, $d) = @_;
+    my $z = linalg::zeros(scalar @{$x});
+    for (my $i = 0; $i < (scalar @{$x}); $i++) {
+        @{$z}[$i] = f($ftype, @{$x}[$i], @{$y}[$i], $a, $b, $c, $d);
+    }
+    return $z;
 }
 
 ################################################################################
 
-io::saveArray("$dataDir\\f.txt", eff($x, $y));
+io::saveArray("$dataDir\\f.txt", eff($ftype, $x, $y, $a, $b, $c, $d));
 
-io::saveArray("$dataDir\\fe.txt", eff($xe, $ye));
+io::saveArray("$dataDir\\fe.txt", eff($ftype, $xe, $ye, $a, $b, $c, $d));
 
